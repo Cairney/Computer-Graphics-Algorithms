a1743f518cd227eb7189f363481501aee260876c
Connor Cairney <connorcairney@hotmail.com>
2018-12-05 10:14:21 +0000

    Add files via upload

---
+++ example1/Viewer.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef Viewer_hpp
+#define Viewer_hpp
+
+#include <stdio.h>
+
+class Viewer {
+public:
+	int view(void);
+};
+
+#endif
---
+++ example1/example1.cpp
@@ -0,0 +1,16 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include "Viewer.hpp"
+
+
+int main(void)
+{
+
+	Viewer viewer = Viewer();
+	viewer.view();
+
+}
+
+
+
